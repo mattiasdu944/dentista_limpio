6fc613a5b87387a7364842bf2ce5e124265fd119
Mattias <duartemattias3@gmail.com>
2022-06-10 22:14:44 -0400

    se agrego los administradores para pacientes, citas y consultorios

--- admCitas.php
+++ admCitas.php
@@ -1,19 +1,19 @@
 <?php
     require_once("vendor/autoload.php");
 ?>
-<!--uwu -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Pacientes</title>
+    <title>Administrador de Citas</title>
 </head>
 <body>
     <?php include('parcials/sidebar.php');?>
     <main class="container section">
-        <h1>Lista de Pacientes</h1>
+        <h1>Registro de Citas</h1>
     </main>
 </body>
 </html>
